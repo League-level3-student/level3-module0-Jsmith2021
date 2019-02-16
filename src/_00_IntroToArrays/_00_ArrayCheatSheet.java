a38b708e994b03763203358f526b1543db1ab8a9
League <league@WTS8.attlocal.net>
2019-02-16 15:57:05 -0800

    2/16/19

--- src/_00_IntroToArrays/_00_ArrayCheatSheet.java
+++ src/_00_IntroToArrays/_00_ArrayCheatSheet.java
@@ -1,26 +1,42 @@
 package _00_IntroToArrays;
 
 import java.util.Random;
+import java.util.ArrayList;
+import java.util.Random;
+
 
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
-
+String[] fruit = new String[5];
+fruit[0]="Mango";
+fruit[1]="Pineapple";
+fruit[2]="Strawberry";
+fruit[3]="Pear";
+fruit[4]="Apple";
 		//2. print the third element in the array
-
+System.out.println(fruit[fruit.length-3]);
 		//3. set the third element to a different value
-
+fruit[2]="Blueberry";
 		//4. print the third element again
-		
+System.out.println(fruit[fruit.length-3]);
 		//5. use a for loop to set all the elements in the array to a string of your choice
-		
+		for (int i = 0; i < fruit.length; i++) {
+			fruit[i]="Passionfruit";
+		}
 		//6. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
+		for (int i = 0; i < fruit.length; i++) {
+			System.out.println(fruit[i]);
+		}
 		
 		//7. make an array of 50 integers
-
+		for (int i = 0; i < 51; i++) {
+			ArrayList<Integer> numbers = new ArrayList<Integer>();
+		
 		//8. use a for loop to make every value of the integer array a random number
-
+		new Random(i).nextInt();
+		}
 		//9. without printing the entire array, print only the smallest number on the array
 
 		//10 print the entire array to see if step 8 was correct
